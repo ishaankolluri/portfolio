976a6ee97978370bfb8cad2ab7db9e620ed79ada
Ishaan Kolluri <ishaankolluri@gmail.com>
2017-12-21 18:55:54 +1300

    Added more snowbuddy portfolio

--- src/components/snowbuddy.js
+++ src/components/snowbuddy.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Row, Col } from 'reactstrap';
+import {Row, Col} from 'reactstrap';
 import '../styles/animate.css';
 
 import '../styles/button.css';
@@ -59,10 +59,11 @@ export default class Snowbuddy extends Component {
           <div className="a-head">Our project:</div>
           <p className="body">Our goal was to design a responsive website to <b>connect people in times of emergency</b>. 
           We purposefully kept the definition loose—we didn't want to begin designing a solution before figuring out our user needs.
-          Our timeline went as follows:</p>
-          <div className="centeredContainer">
-            <ol>
-              <li className="centered-list">Guerrilla research through convenience sampling on the street</li>
+          Our timeline went as follows:
+          </p>
+          <div className="body">
+            <ol className="numberList">
+              <li className="centered-list">Guerrilla research near CMU campus</li>
               <li className="centered-list">Synthesizing personas and storyboarding</li>
               <li className="centered-list">Wireframing</li>
               <li className="centered-list">High fidelity design</li>
@@ -73,13 +74,60 @@ export default class Snowbuddy extends Component {
           <div className="a-head">Initial research:</div>
         </Row>
         <Row>
-          <Col lg="3"><img className="img-fluid portfolio-img" alt="guerrilla research" src="https://s3.us-east-2.amazonaws.com/ishaankolluri/snowbuddy_guerrilla.JPG" /></Col>
-          <Col lg="9">
-            <p>
-              We wanted to help 
-            </p>
-          </Col>
+          <p className="body">
+            Our first exercise was to better understand the types of emergencies out there in a systematic fashion, we drew up a small map of the trajectories we were interested in. 
+            None of us were familiar with a software-driven solution to emergencies, so we decided to put ink to paper first.
+          </p>
         </Row>
+        <Row>
+          <img className="medium-image-inline img-fluid" src="https://s3.us-east-2.amazonaws.com/ishaankolluri/snowbuddyhero.jpeg" alt="header"/>
+        </Row>
+        <Row>
+          <Col lg="9">
+            <p className="body-left">
+              We wanted to create a way for neighbors to help each other during emergencies. However, the three of us haven't been through all conceivable emergencies in the world. 
+              We took to the streets of Pittsburgh to poll our surroundings, asking them about past emergency experiences, what's bothered them the most, and what they wish had happened before and after the event. 
+            </p>
+            <p className="body-left">
+              Our primary takeaway was that <b>it was really unlikely people would be willing to help each other during an actual emergency.</b> Some insights we received were:
+            </p>
+            <div className="body">
+            <ul className="bulletList">
+              <li className="bullet">"I'd be too scared for my own safety to help others in emergency situations".</li>
+              <li className="bullet">"I'd much rather help others in less dangerous situations, or after an emergency situation has already happened."</li>
+              <li className="bullet">"The things I need help with after an emergency are generally tasks I don't have the strength or experience to accomplish."</li>
+            </ul>
+            </div>
+          </Col>
+          <Col lg="3"><img className="img-fluid portfolio-img" alt="guerrilla research" src="https://s3.us-east-2.amazonaws.com/ishaankolluri/snowbuddy_guerrilla.JPG" /></Col>
+        </Row>
+        <Row>
+          <div className="a-head">Scenarios, project scope, and user personas:</div>
+        </Row>
+        <Row>
+          <div>
+            <p className="body">
+              After consolidating the results of 10-12 guerrilla interviews, we crafted a series of <b>user scenarios </b>
+              focused on creating a peer economy for individuals to help each other with emergencies. 
+              We tested these scenarios with a variety of individuals to see whether any of them resonated with a majority. 
+              We asked young students, strangers with children, teachers, and the elderly. 
+              After discussing our feedback to the litany of emergency situations we created scenarios for, we decided to scope our solution: 
+            </p>
+            <p className="body">
+              <b>A peer economy for neighbors to help each other in snow-related emergencies. More specifically, helping people shovel driveways, 
+              salt roads, and break ice, so they may get on with their day.</b>
+            </p>
+          </div>
+        </Row>
+        <Row>
+          <p className="body">
+            After tentatively choosing our solution, we crafted 2 personas that we felt represented our solution's user base.
+          </p>
+        </Row>
+        <Row>
+          <Col lg="6"><img className="portfolio-img img-fluid" src="https://s3.us-east-2.amazonaws.com/ishaankolluri/snowbuddyhero.jpeg" alt="header"/></Col>
+          <Col lg="6"><img className="portfolio-img img-fluid" src="https://s3.us-east-2.amazonaws.com/ishaankolluri/snowbuddyhero.jpeg" alt="header"/></Col> 
+        </Row>        
       </div>
     );
   }
